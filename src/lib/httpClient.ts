e48e55a8cbc413ceed2a51bbb225b53d2834029c
Javiera Saavedra <javiera.saavedra@uc.cl>
2023-06-12 01:26:31 -0400

    fix(app): remove api keys

--- src/lib/httpClient.ts
+++ src/lib/httpClient.ts
@@ -1,8 +1,5 @@
 import axios from 'axios';
 
-const oskyApiKey = 'be3acf29f3msh50d48cffd1bae67p1fd9cajsn8500f77cd705'
-const benApiKey = 'd1bacda54dmshd0f8781440b0905p174b5djsnd8c0fc99c9fb'
-
 export const apiClient = axios.create({
   baseURL: 'https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/',
   headers: {

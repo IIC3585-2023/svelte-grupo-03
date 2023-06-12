7ed3d98d72da7c24e7dc5cd47e84561ce41024cb
Javiera Saavedra <javiera.saavedra@uc.cl>
2023-06-12 01:28:22 -0400

    fix(api-client): remove api key

--- src/lib/httpClient.ts
+++ src/lib/httpClient.ts
@@ -1,10 +1,11 @@
 import axios from 'axios';
+import { OSKY_API_KEY } from './env';
 
 export const apiClient = axios.create({
   baseURL: 'https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/',
   headers: {
     Accept: 'application/json',
-    'X-RapidAPI-Key': 'd1bacda54dmshd0f8781440b0905p174b5djsnd8c0fc99c9fb',
+    'X-RapidAPI-Key': OSKY_API_KEY,
     'X-RapidAPI-Host': 'spoonacular-recipe-food-nutrition-v1.p.rapidapi.com'
   }
 });
